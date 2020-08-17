c0b5685d6d7688cfd4c46891be4654b6f26b7df6
fadikidess <52216213+fadikidess@users.noreply.github.com>
2020-08-17 22:41:27 +0300

    Update run.py

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 import time
 from instabot import Bot
 import os
+import psycopg2
 url = "https://www.flickr.com/photos/nasa2explore/page"
 urls = {}
 txts = {}
@@ -14,6 +15,15 @@ tracker = 1
 nexts = False
 bot = Bot() 
 bot.login(username = "spaceflightstream",password = "FGKpro2003") 
+
+
+DATABASE_URL = os.environ['postgres://cdlxvjkiuieoeu:d0487bbce60768a4e6e96831b8b3e6cb7d98b93d2ddc4ebd16410d098fad8761@ec2-52-22-216-69.compute-1.amazonaws.com:5432/d9i3n82aq3qb6v']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+conn = psycopg2.connect(database = "d9i3n82aq3qb6v", user = "cdlxvjkiuieoeu", password = "d0487bbce60768a4e6e96831b8b3e6cb7d98b93d2ddc4ebd16410d098fad8761", host = "ec2-52-22-216-69.compute-1.amazonaws.com", port = "5432")
+print("Opened database successfully")
+postgreSQL_select_Query = "select * from urls"
+cur = conn.cursor()
 def execute():
     global url
     global tracker
@@ -51,6 +61,24 @@ def execute():
     print(len(txts))
 
     for ind, r in enumerate(txts):
+        tempdr = False
+        cur.execute(postgreSQL_select_Query)
+        records = cur.fetchall() 
+        for row in records:
+            if row[0] == urls[ind]:
+                tempdr = True
+        if tempdr == False:
+            urllib.request.urlretrieve(urls[ind], os.getcwd() + '/space.jpg')
+            cap = txts[ind] + "\n\n\nCredits: " + uro + "\n\n\n #space#nasa#spacex#elon#elonmusk#astronout#engineer#adventure#epic#cool#launch"
+            bot.upload_photo(os.getcwd() + '/space.jpg',caption = cap)
+            print(txts[ind])
+            print(urls[ind])
+            time.sleep(2400)
+            po = """ INSERT INTO urls (url) VALUES (%s)"""
+            record_to_insert = (urls[ind])
+            cur.execute(po, record_to_insert)
+
+            conn.commit()
         with open("Memory.txt", "r+") as myfile:
 
             # A file is an iterable of lines, so this will
@@ -60,12 +88,7 @@ def execute():
                 # Write it; assumes file ends in "\n" already
                 myfile.write(urls[ind]+"\n")
                 myfile.flush()
-                urllib.request.urlretrieve(urls[ind], os.getcwd() + '/space.jpg')
-                cap = txts[ind] + "\n\n\nCredits: " + uro + "\n\n\n #space#nasa#spacex#elon#elonmusk#astronout#engineer#adventure#epic#cool#launch"
-                bot.upload_photo(os.getcwd() + '/space.jpg',caption = cap)
-                print(txts[ind])
-                print(urls[ind])
-                time.sleep(2400)
+                
     else:
         tracker = tracker + 1
         nexts = True
