c5885bd0bc48f0d2a94b785339216f4c553a6cb0
fadikidess <52216213+fadikidess@users.noreply.github.com>
2020-12-21 16:40:45 +0200

    Update runc.py

--- runc.py
+++ runc.py
@@ -14,7 +14,7 @@ txts = {}
 tracker = 4
 nexts = False
 bot = Bot() 
-bot.login(username = "spaceflightstream",password = "") 
+bot.login(username = "spaceflightstream",password = "mypassword") 
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
